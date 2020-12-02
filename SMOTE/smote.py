681fee35e903ca22fd7526c8e8bf227344433dcb
Tucker Guen <tkg11@case.edu>
2020-12-02 00:14:56 -0500

    add kmeans smote

--- SMOTE/smote.py
+++ SMOTE/smote.py
@@ -1,7 +1,28 @@
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 from numpy.random import Generator, PCG64
+from sklearn.cluster import KMeans
 import warnings
+
+
+def kmeans_SMOTE(T, T2, p, k, n_clusters):
+    # Partition T2 into clusters
+    kmeans = KMeans(n_clusters=n_clusters, init='k-means++').fit(T2)
+    # Create clusters from labels
+    clusters = []
+    for c in range(n_clusters):
+        clusters.append([list(ex) for i, ex in enumerate(T2) if kmeans.labels_[i] == c])
+
+    T2o = []
+    # Apply smote to all clusters
+    for cluster in clusters:
+        addtl_samples = SMOTE(cluster, p, k)
+        T2o.extend(addtl_samples)
+
+    for ex in T2o:
+        ex.append(-1)
+    T2o = np.array(T2o)
+    return np.vstack((T, T2o)), T2o
 
 
 def SMOTE(T2, p, k, T1=None, BLL=False):
@@ -27,7 +48,7 @@ def SMOTE(T2, p, k, T1=None, BLL=False):
     # Later, it assumes that N%100 == 0
     if p < 100:
         np.random.shuffle(T2)
-        N2 = (p / 100) * N2
+        N2 = int((p / 100) * N2)
         p = 100
 
     # Num synthetic samples to generate per minority sample
@@ -41,9 +62,9 @@ def SMOTE(T2, p, k, T1=None, BLL=False):
         # Get ith sample
         t = T2[i]
         # array indices of k-nearest neighbors for t
-        nnarray = knn.kneighbors([t])[1]
+        nnarray = knn.kneighbors([t])[1][0]
         # Array of nearest neighbor points
-        nearest = [T2[i] for i in nnarray][0]
+        nearest = [T2[i] for i in nnarray]
         # Generate synthetic samples for point t
         if BLL:
             new_samples = sample_synth_BLL(T1, t, nearest, synths_per_sample)
--- smote_main.py
+++ smote_main.py
@@ -1,4 +1,4 @@
-from SMOTE.smote import SMOTE_dataset
+from SMOTE.smote import SMOTE_dataset, kmeans_SMOTE
 from data_help.data_help import gen_data, split_by_label, plot_dataset
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,7 +8,10 @@ def main():
     T, T1, T2 = gen_data(1000, 2, 19)
     plot_dataset(T)
     plt.show()
-    T_smote, T2_smote = SMOTE_dataset(T, T2, 1000, 5, T1=T1, BLL=True)
+    T_smotek, T2_smotek = kmeans_SMOTE(T, T2, 500, 5, int(len(T2)/15))
+    T_smote, T2_smote = SMOTE_dataset(T, T2, 500, 5)
+    plot_dataset(T_smotek)
+    plt.show()
     plot_dataset(T_smote)
     plt.show()
 
